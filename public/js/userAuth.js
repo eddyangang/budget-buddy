023663cdc3ff8d01171ceecce6182c8b55fdc52e
Royce Williams <roycejames1130@gmail.com>
2020-07-30 13:43:00 -0700

    Fixed firebase errors

--- public/js/userAuth.js
+++ public/js/userAuth.js
@@ -1,141 +1,133 @@
 // Your web app's Firebase configuration
+
 var firebaseConfig = {
-    apiKey: "AIzaSyA5K0WIsrKi0AOFWlVW1MKbPMJsPKNVNVM",
-    authDomain: "budget-buddy-284820.firebaseapp.com",
-    databaseURL: "https://budget-buddy-284820.firebaseio.com",
-    projectId: "budget-buddy-284820",
-    storageBucket: "budget-buddy-284820.appspot.com",
-    messagingSenderId: "924356233283",
-    appId: "1:924356233283:web:146401f6025db366d42cf6",
-    measurementId: "G-7P1831VXX0"
+  apiKey: "AIzaSyA5K0WIsrKi0AOFWlVW1MKbPMJsPKNVNVM",
+  authDomain: "budget-buddy-284820.firebaseapp.com",
+  databaseURL: "https://budget-buddy-284820.firebaseio.com",
+  projectId: "budget-buddy-284820",
+  storageBucket: "budget-buddy-284820.appspot.com",
+  messagingSenderId: "924356233283",
+  appId: "1:924356233283:web:146401f6025db366d42cf6",
+  measurementId: "G-7P1831VXX0"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+// function made to check state of whether or not there is a current user on load
+var failureFlag = true;
 var currentUser;
-// for signing into a new account
+function checkState(){
+  console.log("hello... checking for user signed in...")
+  var user = firebase.auth().currentUser;
+  if(user){
+    console.log("found user");
+    firebase.auth().signOut();
+  }
+}
+ // for signing into a new account
 $("#sign-in-form").on("submit",function(event){
-    // event.stopPropagation();
-    event.preventDefault();
-    var userEmail = $("#userEmail").val().trim();
-    console.log(userEmail);
-    var userPassword = $("#userPassword").val().trim();
-    console.log(userPassword);
-
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-        // ...
-      });
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            currentUser = {
-                userEmail: user.email,
-                id: user.uid
-            };
-            console.log(currentUser);
-
-          // ...
-          console.log("successfully logged into account with a UID of " + user.uid);
+  checkState();
+  // event.stopPropagation();
+  event.preventDefault();
+  var userEmail = $("#userEmail").val().trim();
+  console.log(userEmail);
+  var userPassword = $("#userPassword").val().trim();
+  console.log(userPassword);
+  failureFlag = true;
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      if(errorCode==="auth/invalid-email"){
+        failureFlag = false;
+        return alert("Invalid email");
+      }
+      if(errorCode==="auth/user-disabled"){
+        failureFlag = false;
+        return alert("User disabled");
+      }
+      if(errorCode==="auth/user-not-found"){
+        failureFlag = false;
+        return alert("User not found.");
+      }
+      if(errorCode==="auth/wrong-password"){
+        failureFlag = false;
+        return alert("Incorrect password.");
+      }
+      }).then(function(user){
+        console.log(failureFlag);
+        if(failureFlag===true){
+          console.log(user);
+          currentUser = firebase.auth().currentUser;
+          console.log(currentUser);
+          var passedUser = {
+            displayName: currentUser.displayName,
+            email: currentUser.uid,
+            id:currentUser.uid
+          }
+          console.log("successfully logged into account with a UID of " );
           $.ajax("/user", {
             type: "POST",
-            data: currentUser
-        }).then(
+            data: passedUser
+          }).then(
             function() {
-            console.log("Successfully logged in.");
-            location.replace("/user/home");
-            }
-        );
-        } else {
-          // User is signed out.
-          // ...
+              console.log("Successfully logged in.");
+              location.replace("/user/home");
+          });
         }
       });
 })
 
 // For Creating a new account
 $("#sign-up-form").on("submit",function(event){
-    event.preventDefault();
-    var userName = $("#userName").val().trim();
-    console.log(userName);
-    var userEmail = $("#newUserEmail").val().trim();
-    console.log(userEmail);
-    var userPassword = $("#newUserPassword").val().trim();
-    console.log(userPassword);
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
+  event.preventDefault();
+  var userName = $("#userName").val().trim();
+  console.log(userName);
+  var newUserEmail = $("#newUserEmail").val().trim();
+  console.log(newUserEmail);
+  var newUserPassword = $("#newUserPassword").val().trim();
+  console.log(newUserPassword);
+  firebase.auth().createUserWithEmailAndPassword(newUserEmail, newUserPassword).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      if(errorCode==="auth/invalid-email"){
+        return alert("Invalid email");
+      }
+      if(errorCode==="auth/user-disabled"){
+        return alert("User disabled");
+      }
+      if(errorCode==="auth/user-not-found"){
+        return alert("User not found.");
+      }
+      if(errorCode==="auth/wrong-password"){
+        return alert("Incorrect password.");
+      }
+  }).then(function(user){
+      console.log(user);
+      currentUser = firebase.auth().currentUser;
+      console.log(currentUser);
+      // Passing through the displayName userId and Email
+      var passedUser = {
+        displayName: currentUser.displayName,
+        email: currentUser.email,
+        id:currentUser.uid
+      }
+      console.log("successfully logged into account with a UID of " );
+      $.ajax("/user/new", {
+        type: "POST",
+        data: passedUser
+      }).then(
+        function() {
+          console.log("Successfully logged in.");
+          location.replace("/user/home");
       });
-    //   after new account is created...
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            currentUser = {
-                userEmail: user.email,
-                id: user.uid
-            };
-            console.log(currentUser);
-
-          // ...
-          console.log("successfully logged into account with a UID of " + user.uid);
-          $.ajax("/user", {
-            type: "POST",
-            data: currentUser
-        }).then(
-            function() {
-            console.log("Successfully logged in.");
-            location.replace("/user/home");
-            }
-        );
-        } else {
-          // User is signed out.
-          // ...
-        }
-      });
-      
+    });
+  
+  //   after new account is created...
+    
 });
-
-
-// Initialize the FirebaseUI Widget using Firebase.
-// var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// var uiConfig = {
-//     callbacks: {
-//       signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-//         // User successfully signed in.
-//         // Return type determines whether we continue the redirect automatically
-//         // or whether we leave that to developer to handle.
-//         user = firebase.auth().currentUser
-//         console.log(user);
-//         console.log(authResult);
-//         if(authResult)
-//         console.log(redirectUrl);
-//         // return true;
-//       },
-//       uiShown: function() {
-//         // The widget is rendered.
-//         // Hide the loader.
-//         document.getElementById('loader').style.display = 'none';
-//       }
-//     },
-//     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-//     signInFlow: 'popup',
-//     signInSuccessUrl: '<url-to-redirect-to-on-success>',
-//     signInOptions: [
-//       // Leave the lines as is for the providers you want to offer your users.
-//     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
-//       firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
-//     ],
-//     // Terms of service url.
-//     tosUrl: '<your-tos-url>',
-//     // Privacy policy url.
-//     privacyPolicyUrl: '<your-privacy-policy-url>'
-//   };
-// ui.start('#firebaseui-auth-container',uiConfig);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -22,11 +22,11 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 
 var routes = require("./routes/api-routes.js");
-=======
+// =======
 // var routes = require("./controllers/budgetController.js");
 
 
-// app.use(routes);
+app.use(routes);
 
 
 // Start our server so that it can begin listening to client requests.
