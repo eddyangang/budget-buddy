37c348ec143360bea0790f96cc3b47f871f6ccbe
Giovanni Noisy <gnoisy77@gmail.com>
2020-08-01 13:31:23 -0700

    added code tocalendar.js

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -37,10 +37,20 @@ var calendar = new tui.Calendar(document.getElementById('calendar'), {
     },useCreationPopup: true
 
 });
+//get request to db to get schedules within time frames/range
+//turn db rows into schedule objects
+//append objects to list
+//call create schedules use list
+//sort db rows based on categories
+//find sums for each category
+//get request to pull budgets / calculate difference to display sum
 
-// calendar.on('clickMore', function(event) {
-//     console.log('clickMore', event.date, event.target);
-// });
+
+//user should enter category first then the amount in the schedule title
+//parse category and amount
+//make function that is used in post and put requests
+
+
 
 calendar.on('clickDayname', function(event) {
     if (calendar.getViewName() === 'week') {
@@ -48,3 +58,33 @@ calendar.on('clickDayname', function(event) {
         calendar.changeView('day', true);
     }
 });
+
+console.log(calendar.getDateRangeEnd());
+console.log(calendar.getDateRangeStart());
+
+calendar.on('beforeCreateSchedule', function(event) {
+    console.log("I created an event");
+    //make post request to create new schedule in db
+    var startTime = event.start;
+    var endTime = event.end;
+    var newSchedule = {
+        id: "1",
+        calendarId: "1",
+        category: "time",
+        dueDateClass: "",
+        start: startTime,
+        end: endTime
+    }
+    calendar.createSchedules([newSchedule]);
+})
+
+calendar.on('beforeDeleteSchedule', function(event) {
+    //make delete request to delete schedule
+    console.log("I deleted an event");
+})
+
+
+calendar.on('beforeUpdateSchedule', function(event) {
+    //make put request to update schedule in db
+    console.log("I updated an event");
+})
