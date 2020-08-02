63d2d1fac0468f137f217a62760f05b34c867921
Royce Williams <roycejames1130@gmail.com>
2020-08-02 15:48:40 -0700

    added a new category handlebar for viewing categories

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,32 +9,32 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  router.get("/", function (req, res) {
-    res.render("landing");
-  })
+  // router.get("/", function (req, res) {
+  //   res.render("landing");
+  // })
   
-  router.get("/sign-up", function (req, res) {
-    res.render("sign-up");
-  })
+  // router.get("/sign-up", function (req, res) {
+  //   res.render("sign-up");
+  // })
   
-  router.get("/user/home", function (req, res) {
-    db.Users.findOne({
-      where: {
+  // router.get("/user/home", function (req, res) {
+  //   db.Users.findOne({
+  //     where: {
   
-        id: userSignedIn.id
-      }
-    }).then(function (data) {
-      userID: userSignedIn.id
-    }).then(function (data) {
+  //       id: userSignedIn.id
+  //     }
+  //   }).then(function (data) {
+  //     userID: userSignedIn.id
+  //   }).then(function (data) {
   
-      console.log(data);
-      res.render("index", data);
-    })
-  })
+  //     console.log(data);
+  //     res.render("index", data);
+  //   })
+  // })
   
-  router.get("/user/sign-out", function (req, res) {
-    res.render("landing");
+  // router.get("/user/sign-out", function (req, res) {
+  //   res.render("landing");
   
-  })
+  // })
     
 };
